4937e7043ce5410ff2cdb6cf86c6c1737ce5ce30
André Noordzij <noordzij.am@gmail.com>
2024-11-11 16:13:02 +0100

    FIrst checkin

---
+++ AoC.Pathfinding/Dijkstra.cs
@@ -0,0 +1,34 @@
+﻿namespace AoC.Pathfinding
+{
+	public static class Dijkstra
+	{
+		public static IEnumerable<INode> FindShortestPath(INode start, INode end)
+		{
+			PriorityQueue<INode, int> queue = new PriorityQueue<INode, int>();
+			queue.Enqueue(start, 0);
+			INode? currentNode;
+			while (queue.TryDequeue(out currentNode, out int traveledCost) && !Equals(currentNode, end))
+			{
+				if (currentNode.Visited)
+				{
+					continue;
+				}
+				currentNode.SetVisited(true);
+				foreach ((INode nextNode, int cost) in currentNode.ConnectedNodes)
+				{
+					if (!nextNode.Visited && (nextNode.TraversalCost == 0 || nextNode.TraversalCost < traveledCost + cost))
+					{
+						int newPriority = nextNode.TraversalCost = traveledCost + cost;
+						queue.Enqueue(nextNode, newPriority);
+					}
+				}
+				currentNode = queue.Dequeue();
+			}
+			if(Equals(currentNode, end))
+			{
+				return currentNode.Path;
+			}
+			return Enumerable.Empty<INode>();
+		}
+	}
+}
