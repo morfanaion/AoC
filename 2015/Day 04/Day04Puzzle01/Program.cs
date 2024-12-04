3bcd8bac7b5878a570ca1eb4547b66d5f35a9b93
André Noordzij <a.noordzij@chipsoft.com>
2024-12-04 15:33:03 +0100

    2015 - Day 4

---
+++ 2015/Day 04/Day04Puzzle01/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Security.Cryptography;
+MD5 md5 = MD5.Create();
+int i = 1;
+string input = "iwrupvqb";
+while(!Convert.ToHexString(md5.ComputeHash(System.Text.Encoding.ASCII.GetBytes(input + i.ToString()))).StartsWith("00000"))
+{
+    i++;
+}
+Console.WriteLine(i);
