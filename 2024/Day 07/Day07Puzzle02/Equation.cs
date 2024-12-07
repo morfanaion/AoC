6879515f688ecb08484f077923cfdacd462eac6d
André Noordzij <noordzij.am@gmail.com>
2024-12-07 07:38:33 +0100

    Day 7 2024

---
+++ 2024/Day 07/Day07Puzzle02/Equation.cs
@@ -0,0 +1,40 @@
+﻿namespace Day07Puzzle02
+{
+	internal class Equation(long[] numbers)
+	{
+		public static Equation FromString(string value)
+		{
+			string[] parts = value.Split(": ", StringSplitOptions.RemoveEmptyEntries);
+			return new Equation(parts[1].Split(' ').Select(long.Parse).ToArray())
+			{
+				Test = long.Parse(parts[0])
+			};
+		}
+
+		public long Test { get; set; }
+		public long[] Numbers { get; set; } = numbers;
+
+		public bool PossiblyTrue => PossibleRecurse(Numbers[0], Numbers.Skip(1));
+
+		private bool PossibleRecurse(long resultTillNow, IEnumerable<long> numbersToProcess)
+		{
+			if (resultTillNow == Test && !numbersToProcess.Any())
+			{
+				return true;
+			}
+			if(resultTillNow > Test)
+			{
+				return false;
+			}
+			if(numbersToProcess.Any() && 
+				(
+					PossibleRecurse(resultTillNow + numbersToProcess.First(), numbersToProcess.Skip(1)) ||
+					PossibleRecurse(resultTillNow * numbersToProcess.First(), numbersToProcess.Skip(1)) ||
+					PossibleRecurse(long.Parse($"{resultTillNow}{numbersToProcess.First()}"), numbersToProcess.Skip(1))))
+			{
+				return true;
+			}
+			return false;
+		}
+	}
+}
---
+++ 2024/Day 07/Day07Puzzle02/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Day07Puzzle02;
+
+Console.WriteLine(File.ReadAllLines("input.txt").Select(Equation.FromString).Where(e => e.PossiblyTrue).Sum(e => e.Test));
