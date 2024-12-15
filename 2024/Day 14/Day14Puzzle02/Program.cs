1991213c6e41c5753dc09684114e92a3911eb9f3
André Noordzij <noordzij.am@gmail.com>
2024-12-15 17:05:16 +0100

    Generelized day 14 2024 to find the answer for any input, rather than displaying the answer that I found through searching the output of every second.

--- 2024/Day 14/Day14Puzzle02/Program.cs
+++ 2024/Day 14/Day14Puzzle02/Program.cs
@@ -2,15 +2,37 @@
 
 Robot[] robots = File.ReadAllLines("input.txt").Select(Robot.FromString).ToArray();
 
-foreach(Robot robot1 in robots)
+int iteration = 0;
+while (!TreeFound())
 {
-	robot1.Progress(6577);
+    iteration++;
+    foreach (Robot robot1 in robots)
+    {
+        robot1.Progress(1);
+    }
 }
-PrintState(6577, robots);
+
+// 6577
+PrintState(iteration, robots);
+
+bool TreeFound()
+{
+    IEnumerable<IGrouping<long, Robot>> robotsByLine = robots.GroupBy(r => r.Y);
+    return robotsByLine.Any(g => IsPartOfTreeLogo(g));
+}
+
+bool IsPartOfTreeLogo(IGrouping<long, Robot> g)
+{
+    char[] line = Enumerable.Range(0, Robot.BathroomWidth).Select(i => '.').ToArray();
+    foreach(long x in g.Select(r => r.X).Distinct())
+    {
+        line[x] = '0';
+    }
+    return string.Concat(line).Contains("000000000000000000");
+}
 
 void PrintState(int iteration, Robot[] robots)
 {
-    Console.WriteLine(iteration);
     char[][] bathroom = Enumerable.Range(0, Robot.BathroomHeight).Select(j => Enumerable.Range(0, Robot.BathroomWidth).Select(i => '.').ToArray()).ToArray();
     foreach (Robot robot in robots)
     {
@@ -18,4 +40,5 @@ void PrintState(int iteration, Robot[] robots)
     }
     Console.WriteLine(string.Join('\n', bathroom.Select(line => string.Join("", line))));
     Console.WriteLine();
+    Console.WriteLine(iteration);
 }
