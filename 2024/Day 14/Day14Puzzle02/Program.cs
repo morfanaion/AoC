f3f4ba61f80bacb4b72cea1084e5cae09f4cb54b
André Noordzij <noordzij.am@gmail.com>
2024-12-15 14:40:17 +0100

    Day 24 temp fix

--- 2024/Day 14/Day14Puzzle02/Program.cs
+++ 2024/Day 14/Day14Puzzle02/Program.cs
@@ -2,19 +2,20 @@
 
 Robot[] robots = File.ReadAllLines("input.txt").Select(Robot.FromString).ToArray();
 
-foreach (Robot robot in robots)
+foreach(Robot robot1 in robots)
 {
-	robot.Progress(37786);
+	robot1.Progress(6577);
 }
-PrintState(robots);
+PrintState(6577, robots);
 
-void PrintState(Robot[] robots)
+void PrintState(int iteration, Robot[] robots)
 {
-	char[][] bathroom = Enumerable.Range(0, Robot.BathroomHeight).Select(j => Enumerable.Range(0, Robot.BathroomWidth).Select(i => '.').ToArray()).ToArray();
-	foreach(Robot robot in robots)
-	{
-		bathroom[robot.Y][robot.X] = '0';
-	}
-	Console.WriteLine(string.Join('\n', bathroom.Select(line => string.Join("", line))));
+    Console.WriteLine(iteration);
+    char[][] bathroom = Enumerable.Range(0, Robot.BathroomHeight).Select(j => Enumerable.Range(0, Robot.BathroomWidth).Select(i => '.').ToArray()).ToArray();
+    foreach (Robot robot in robots)
+    {
+        bathroom[robot.Y][robot.X] = '0';
+    }
+    Console.WriteLine(string.Join('\n', bathroom.Select(line => string.Join("", line))));
     Console.WriteLine();
 }
