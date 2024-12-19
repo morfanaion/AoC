0c9caceccca5d9da1e32d99b4efcaf67627d4fd5
André Noordzij <a.noordzij@chipsoft.com>
2024-12-19 10:40:52 +0100

    Day 19 2024

---
+++ 2024/Day 19/Day19Puzzle01/Program.cs
@@ -0,0 +1,18 @@
+﻿string[] input = File.ReadAllLines("input.txt");
+string[] towels = input[0].Split(',').Select(t => t.Trim()).OrderByDescending(t => t.Length).ToArray();
+
+Dictionary<string, bool> possibleDictionary = new Dictionary<string, bool>
+{
+    { string.Empty, true }
+};
+
+Console.WriteLine(input.Skip(2).Count(DesignPossible));
+
+bool DesignPossible(string design)
+{
+    if (possibleDictionary.TryGetValue(design, out bool result))
+    {
+        return result;
+    }
+    return possibleDictionary[design] = towels.Any(towel => design.StartsWith(towel) && DesignPossible(design.Substring(towel.Length)));
+}
---
+++ 2024/Day 19/Day19Puzzle02/Program.cs
@@ -0,0 +1,18 @@
+﻿string[] input = File.ReadAllLines("input.txt");
+string[] towels = input[0].Split(',').Select(t => t.Trim()).OrderByDescending(t => t.Length).ToArray();
+
+Dictionary<string, long> possibleDictionary = new Dictionary<string, long>
+{
+    { string.Empty, 1 }
+};
+Console.WriteLine(input.Skip(2).Sum(DesignPossible));
+
+
+long DesignPossible(string design)
+{
+    if (possibleDictionary.TryGetValue(design, out long result))
+    {
+        return result;
+    }
+    return possibleDictionary[design] = towels.Sum(towel => design.StartsWith(towel) ? DesignPossible(design.Substring(towel.Length)) : 0);
+}
