121ed438385b2e0f1bc9902dcc4eebbe6f84ea42
André Noordzij <noordzij.am@gmail.com>
2024-12-24 07:54:26 +0100

    Day 24 Puzzle 1 2024

--- 2024/Day 24/Day24Puzzle01/Gate.cs
+++ 2024/Day 24/Day24Puzzle01/Gate.cs
@@ -2,6 +2,7 @@
 {
 	internal abstract class Gate
 	{
+		public static List<Gate> Gates = new List<Gate>();
 		public string Id { get; set; } = string.Empty;
 		public abstract bool Output { get; }
 	}
---
+++ 2024/Day 24/Day24Puzzle01/Program.cs
@@ -0,0 +1,84 @@
+﻿using Day24Puzzle01;
+using System.Text.RegularExpressions;
+
+Dictionary<string, List<Action<Gate>>> queuedGateActions = new Dictionary<string, List<Action<Gate>>>();
+Action<string> processLine = line =>
+{
+    if (!string.IsNullOrEmpty(line))
+    {
+        Match staticMatch = RegExHelper.GetStaticGateRegex().Match(line);
+        Gate.Gates.Add(new StaticGate()
+        {
+            Id = staticMatch.Groups["gateId"].Value,
+            Input = staticMatch.Groups["output"].Value == "1",
+        });
+    }
+    else
+    {
+        processLine = line =>
+        {
+            Match nonStaticMatch = RegExHelper.GetNonStaticGateRegex().Match(line);
+            NonStaticGate gate = nonStaticMatch.Groups["operator"].Value switch
+            {
+                "XOR" => new XorGate(),
+                "AND" => new AndGate(),
+                "OR" => new OrGate(),
+                _ => throw new InvalidDataException("Unsupported operator found")
+            };
+            gate.Id = nonStaticMatch.Groups["gateId"].Value;
+            if(Gate.Gates.FirstOrDefault(g => g.Id == nonStaticMatch.Groups["gate1"].Value) is Gate input1Gate)
+            {
+                gate.Input1 = input1Gate;
+            }
+            else
+            {
+                if(queuedGateActions.TryGetValue(nonStaticMatch.Groups["gate1"].Value, out List<Action<Gate>>? setGateList))
+                {
+                    setGateList.Add(g => gate.Input1 = g);
+                }
+                else
+                {
+                    queuedGateActions.Add(nonStaticMatch.Groups["gate1"].Value, new List<Action<Gate>>() { g => gate.Input1 = g });
+                }
+            }
+            if (Gate.Gates.FirstOrDefault(g => g.Id == nonStaticMatch.Groups["gate2"].Value) is Gate input2Gate)
+            {
+                gate.Input2 = input2Gate;
+            }
+            else
+            {
+                if (queuedGateActions.TryGetValue(nonStaticMatch.Groups["gate2"].Value, out List<Action<Gate>>? setGateList))
+                {
+                    setGateList.Add(g => gate.Input2 = g);
+                }
+                else
+                {
+                    queuedGateActions.Add(nonStaticMatch.Groups["gate2"].Value, new List<Action<Gate>>() { g => gate.Input2 = g });
+                }
+            }
+            if(queuedGateActions.TryGetValue(gate.Id, out List<Action<Gate>>? mySetGateList))
+            {
+                foreach(var setter in mySetGateList)
+                {
+                    setter(gate);
+                }
+            }
+            Gate.Gates.Add(gate);
+        };
+    }
+};
+foreach(string input in File.ReadAllLines("input.txt"))
+{
+    processLine(input);
+}
+
+ulong result = 0;
+foreach(Gate gate in Gate.Gates.Where(g => g.Id.StartsWith("z")).OrderByDescending(g => g.Id))
+{
+    result <<= 1;
+    if(gate.Output)
+    {
+        result++;
+    }
+}
+Console.WriteLine(result);
