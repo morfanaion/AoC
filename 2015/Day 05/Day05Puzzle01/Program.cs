5863248e709ca6c16cde3fd00760b2c2550ca1ce
André Noordzij <noordzij.am@gmail.com>
2025-04-14 15:08:43 +0200

    Day 5 2015

---
+++ 2015/Day 05/Day05Puzzle01/Program.cs
@@ -0,0 +1,16 @@
+﻿string[] disallowedCombinations = ["ab", "cd", "pq", "xy"];
+
+Console.WriteLine(File.ReadAllLines("Input.txt").Count(IsNiceString));
+
+bool IsNiceString(string arg)
+{
+	if (!arg.Where("aeoiu".Contains).Skip(2).Any())
+	{
+		return false;
+	}
+	if(!arg.Skip(1).Zip(arg).Any(pair => pair.First == pair.Second))
+	{
+		return false;
+	}
+	return !disallowedCombinations.Any(arg.Contains);
+}
---
+++ 2015/Day 05/Day05Puzzle02/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine(File.ReadAllLines("Input.txt").Count(IsNiceString));
+
+bool IsNiceString(string arg)
+{
+	return arg.Skip(2).Zip(arg).Any(pair => pair.First == pair.Second) && HasDoublePair(arg);
+}
+
+bool HasDoublePair(string arg)
+{
+	for (int i1 = 0; i1 < arg.Length - 3; i1++)
+	{
+		for (int i2 = i1 + 2; i2 < arg.Length - 1; i2++)
+		{
+			if (arg[i1] == arg[i2] && arg[i1 + 1] == arg[i2 + 1])
+			{
+				return true;
+			}
+		}
+	}
+	return false;
+}
