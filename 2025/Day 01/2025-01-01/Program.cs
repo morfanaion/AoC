027646e654229c2abadc446cdeb2b8449651a587
Morfanaion <noordzij.am@gmail.com>
2025-12-01 09:29:26 +0100

    Day 1 2025

---
+++ 2025/Day 01/2025-01-01/Program.cs
@@ -0,0 +1,34 @@
+﻿int currentPos = 50;
+int numTimesAt0 = 0;
+foreach(string line in File.ReadAllLines("input.txt"))
+{
+    Func<int, int, int> movePosition = line[0] switch
+    {
+        'L' => MoveLeft,
+        'R' => MoveRight,
+        _ => throw new InvalidOperationException("Invalid instruction")
+    };
+    currentPos = movePosition(currentPos, int.Parse(line.Substring(1)));
+    if(currentPos == 0)
+    {
+        numTimesAt0++;
+    }
+}
+Console.WriteLine(numTimesAt0);
+
+int MoveLeft(int position, int numPositions) => TrimRound(position - numPositions);
+
+int MoveRight(int position, int numPositions) => TrimRound(position + numPositions);
+
+int TrimRound(int position)
+{
+    while(position < 0)
+    {
+        position += 100;
+    }
+    while(position >= 100)
+    {
+        position -= 100;
+    }
+    return position;
+}
---
+++ 2025/Day 01/2025-01-02/Program.cs
@@ -0,0 +1,50 @@
+﻿int currentPos = 50;
+int numTimesAt0 = 0;
+foreach (string line in File.ReadAllLines("input.txt"))
+{
+    Func<int, int, int> movePosition = line[0] switch
+    {
+        'L' => MoveLeft,
+        'R' => MoveRight,
+        _ => throw new InvalidOperationException("Invalid instruction")
+    };
+    int numClicks = int.Parse(line.Substring(1));
+    numTimesAt0 += numClicks / 100;
+    numClicks %= 100;
+    int newPos = movePosition(currentPos, numClicks);
+    if (currentPos != 0)
+    {
+        if (newPos == 0)
+        {
+            numTimesAt0++;
+        }
+        else if (newPos < 0)
+        {
+            newPos += 100;
+            numTimesAt0++;
+        }
+        else if (newPos >= 100)
+        {
+            newPos -= 100;
+            numTimesAt0++;
+        }
+    }
+    else
+    {
+        if (newPos < 0)
+        {
+            newPos += 100;
+        }
+        else if (newPos >= 100)
+        {
+            newPos -= 100;
+        }
+    }
+    currentPos = newPos;
+}
+Console.WriteLine(numTimesAt0);
+
+int MoveLeft(int position, int numPositions) => position - numPositions;
+
+int MoveRight(int position, int numPositions) => position + numPositions;
+
