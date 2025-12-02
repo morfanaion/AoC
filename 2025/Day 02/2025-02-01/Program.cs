1bd4823931d906f0c37703ba23190935fadfc36c
Morfanaion <noordzij.am@gmail.com>
2025-12-03 00:09:14 +0100

    Day 2 2025 (part 1)

---
+++ 2025/Day 02/2025-02-01/Program.cs
@@ -0,0 +1,58 @@
+﻿(string lowBound, string highBound)[] inputs = File.ReadAllText("input.txt").Split(',').Select(s => s.Split('-')).Select<string[], (string lowBound, string highBound)>(p => new (p[0], p[1])).ToArray();
+ulong result = 0;
+foreach((string lowBound, string highBound) input in inputs)
+{
+    ulong lowBound = ulong.Parse(input.lowBound);
+    ulong highBound = ulong.Parse(input.highBound);
+    ulong startNumberLowBound = 0;
+    ulong valueForStartLowBound = 0;
+    if (input.lowBound.Length % 2 == 0)
+    {
+        ulong diviser = (ulong)Math.Pow(10, input.lowBound.Length / 2);
+        startNumberLowBound = lowBound / diviser;
+        valueForStartLowBound = lowBound % diviser;
+    }
+    else
+    {
+        startNumberLowBound = (ulong)Math.Pow(10, input.lowBound.Length / 2);
+        valueForStartLowBound = 0;
+    }
+    ulong startNumberHighBound = 0;
+    ulong valueForEndHighBound = 0;
+    if (input.highBound.Length % 2 == 0)
+    {
+        ulong diviser = (ulong)Math.Pow(10, input.highBound.Length / 2);
+        startNumberHighBound = highBound / diviser;
+        valueForEndHighBound = highBound % diviser;
+    }
+    else
+    {
+        startNumberHighBound = (ulong)Math.Pow(10, input.highBound.Length / 2) - 1;
+        valueForEndHighBound = startNumberHighBound;
+    }
+    if(startNumberLowBound > startNumberHighBound)
+    {
+        continue;
+    }
+    if(startNumberLowBound == startNumberHighBound)
+    {
+        if(valueForStartLowBound <= startNumberHighBound && valueForEndHighBound >= startNumberHighBound)
+        {
+            result += ulong.Parse($"{startNumberHighBound}{startNumberHighBound}");
+        }
+        continue;
+    }
+    if(valueForStartLowBound <= startNumberLowBound)
+    {
+        result += ulong.Parse($"{startNumberLowBound}{startNumberLowBound}");
+    }
+    if(valueForEndHighBound >= startNumberHighBound)
+    {
+        result += ulong.Parse($"{startNumberHighBound}{startNumberHighBound}");
+    }
+    for(ulong i = startNumberLowBound + 1; i < startNumberHighBound; i++)
+    {
+        result += ulong.Parse($"{i}{i}");
+    }
+}
+Console.WriteLine(result);
---
+++ 2025/Day 02/2025-02-02/Program.cs
@@ -0,0 +1,38 @@
+﻿(string lowBound, string highBound)[] inputs = File.ReadAllText("input.txt").Split(',').Select(s => s.Split('-')).Select<string[], (string lowBound, string highBound)>(p => new(p[0], p[1])).ToArray();
+foreach((string lowBound, string highBound) input in inputs)
+{
+    if(input.lowBound.Length == input.highBound.Length)
+    {
+        // same length
+        for (int groupSize = 1; groupSize < input.lowBound.Length - 1; groupSize++)
+        {
+            if (input.lowBound.Length % groupSize != 0)
+            {
+                continue;
+            }
+            ulong lowerBoundStartNum = ulong.Parse(input.lowBound.Substring(0, groupSize));
+            ulong higherBoundStartNum = ulong.Parse(input.highBound.Substring(0, groupSize));
+            ulong lowerBoundRemainder = ulong.Parse(input.lowBound.Substring(groupSize));
+            ulong higherBoundRemainder = ulong.Parse(input.highBound.Substring(groupSize));
+            if (lowerBoundStartNum == higherBoundStartNum)
+            {
+                // check for numbers within range
+                continue;
+            }
+            // check for numbers from 
+        }
+    }
+}
+
+IEnumerable<string> SplitStringBySize(string input, int size)
+{
+    if (input.Length % size != 0)
+    {
+        yield break;
+    }
+    for (int i = 0; i < input.Length; i += size)
+    {
+        yield return input.Substring(i, size);
+    }
+}
+
