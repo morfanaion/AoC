3b67ada79da2c4a69f7d9dc2bd838c4fa015e885
Morfanaion <noordzij.am@gmail.com>
2025-12-03 19:31:19 +0100

    Day 3 2025

---
+++ 2025/Day 03/2025-03-01/Program.cs
@@ -0,0 +1,15 @@
+﻿uint result = 0;
+foreach(string line in File.ReadAllLines("input.txt"))
+{
+    char highest = line.Max();
+    int idx = line.IndexOf(highest);
+    if (idx == line.Length - 1 )
+    {
+        result += (((uint)(line.Substring(0, line.Length - 1).Max() - '0')) * 10) + (uint)(highest - '0');
+    }
+    else
+    {
+        result += (((uint)(highest - '0')) * 10) + (uint)(line.Substring(idx + 1).Max() - '0');
+    }
+    Console.WriteLine(result);
+}
---
+++ 2025/Day 03/2025-03-02/Program.cs
@@ -0,0 +1,16 @@
+﻿ulong result = 0;
+const int numBatteriesToTurnOn = 12;
+foreach(string line in File.ReadAllLines("input.txt"))
+{
+    string turnedOnBatteries = string.Empty;
+    string lineRemaining = line;
+    while(turnedOnBatteries.Length != numBatteriesToTurnOn)
+    {
+        char highest = lineRemaining.Substring(0, lineRemaining.Length - (numBatteriesToTurnOn - 1 - turnedOnBatteries.Length)).Max();
+        int idx = lineRemaining.IndexOf(highest);
+        lineRemaining = lineRemaining.Substring(idx + 1);
+        turnedOnBatteries += highest;
+    }
+    result += ulong.Parse(turnedOnBatteries);
+}
+Console.WriteLine(result);
