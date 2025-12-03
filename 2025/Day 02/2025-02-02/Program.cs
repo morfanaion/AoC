81c20e1b7c0c389f71a4b8acbfa738b8ac616f4f
Morfanaion <noordzij.am@gmail.com>
2025-12-03 19:06:50 +0100

    Day 2 part 2 finally finished...

--- 2025/Day 02/2025-02-02/Program.cs
+++ 2025/Day 02/2025-02-02/Program.cs
@@ -2,6 +2,8 @@
 List<ulong> invalidIds = new List<ulong>();
 foreach ((string lowBound, string highBound) input in inputs)
 {
+    ulong lowBound = ulong.Parse(input.lowBound);
+    ulong highBound = ulong.Parse(input.highBound);
     List<ulong> InvalidIdsForRange = new List<ulong>();
     for (int stringSize = input.lowBound.Length; stringSize <= input.highBound.Length; stringSize++)
     {
@@ -12,69 +14,34 @@ foreach ((string lowBound, string highBound) input in inputs)
                 continue;
             }
             ulong lowerBoundStartNum = (ulong)Math.Pow(10, groupSize - 1);
-            ulong lowerBoundRemainder = 0;
             if (stringSize == input.lowBound.Length)
             {
-                string[] lowBoundGroups = SplitStringBySize(input.lowBound, groupSize).ToArray();
-                lowerBoundStartNum = ulong.Parse(lowBoundGroups[0]);
-                lowerBoundRemainder = lowBoundGroups.Skip(1).Select(ulong.Parse).Max();
+                lowerBoundStartNum = ulong.Parse(input.lowBound.Substring(0, groupSize));
             }
             ulong higherBoundStartNum = (ulong)Math.Pow(10, groupSize) - 1;
-            ulong higherBoundRemainder = higherBoundStartNum;
             if (stringSize == input.highBound.Length)
             {
-                string[] highBoundGroups = SplitStringBySize(input.highBound, groupSize).ToArray();
-                higherBoundStartNum = ulong.Parse(highBoundGroups[0]);
-                higherBoundRemainder = highBoundGroups.Skip(1).Select(ulong.Parse).Min();
+                higherBoundStartNum = ulong.Parse(input.highBound.Substring(0, groupSize));
             }
-            InvalidIdsForRange.AddRange(Process(lowerBoundStartNum, lowerBoundRemainder, higherBoundStartNum, higherBoundRemainder, stringSize / groupSize));
+            invalidIds.AddRange(Process(lowerBoundStartNum, higherBoundStartNum, highBound, lowBound, stringSize / groupSize));
         }
     }
-    invalidIds.AddRange(InvalidIdsForRange.Distinct());
 }
 ulong result = 0;
-foreach(ulong invalidId in invalidIds)
+foreach(ulong invalidId in invalidIds.Distinct())
 {
-       result += invalidId;
+    result += invalidId;
 }
 Console.WriteLine(result);
 
-IEnumerable<string> SplitStringBySize(string input, int size)
+IEnumerable<ulong> Process(ulong startNumberLowBound, ulong startNumberHighBound, ulong highBound, ulong lowBound, int numGroups)
 {
-    if (input.Length % size != 0)
+    for (ulong i = startNumberLowBound; i <= startNumberHighBound; i++)
     {
-        yield break;
-    }
-    for (int i = 0; i < input.Length; i += size)
-    {
-        yield return input.Substring(i, size);
-    }
-}
-
-IEnumerable<ulong> Process(ulong startNumberLowBound, ulong valueForStartLowBound, ulong startNumberHighBound, ulong valueForEndHighBound, int numGroups)
-{
-    if (startNumberLowBound > startNumberHighBound)
-    {
-        yield break;
-    }
-    if (startNumberLowBound == startNumberHighBound)
-    {
-        if (valueForStartLowBound <= startNumberHighBound && valueForEndHighBound >= startNumberHighBound)
+        ulong numberToCompare = ulong.Parse(string.Concat(Enumerable.Repeat(i.ToString(), numGroups)));
+        if (numberToCompare >= lowBound && numberToCompare <= highBound)
         {
-            yield return ulong.Parse(string.Concat(Enumerable.Repeat(startNumberHighBound.ToString(), numGroups)));
+            yield return numberToCompare;
         }
-        yield break;
-    }
-    if (valueForStartLowBound <= startNumberLowBound)
-    {
-        yield return ulong.Parse(string.Concat(Enumerable.Repeat(startNumberLowBound.ToString(), numGroups)));
-    }
-    if (valueForEndHighBound >= startNumberHighBound)
-    {
-        yield return ulong.Parse(string.Concat(Enumerable.Repeat(startNumberHighBound.ToString(), numGroups)));
-    }
-    for (ulong i = startNumberLowBound + 1; i < startNumberHighBound; i++)
-    {
-        yield return ulong.Parse(string.Concat(Enumerable.Repeat(i.ToString(), numGroups)));
     }
 }
