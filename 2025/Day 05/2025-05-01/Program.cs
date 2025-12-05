f9096a3b48be5a5fcd0c30f8a68e447f8e28e136
André Noordzij <a.noordzij@chipsoft.com>
2025-12-05 10:12:43 +0100

    Day 5 2025

---
+++ 2025/Day 05/2025-05-01/Program.cs
@@ -0,0 +1,27 @@
+﻿List<ulong> ids = new List<ulong>();
+List<(ulong low, ulong high)> freshRanges = new List<(ulong low, ulong high)>();
+
+Action<string> processLine = line =>
+{
+    if (string.IsNullOrWhiteSpace(line))
+    {
+        processLine = line =>
+        {
+            ids.Add(ulong.Parse(line));
+        };
+        return;
+    }
+    string[] parts = line.Split('-');
+    ulong low = ulong.Parse(parts[0]);
+    ulong high = ulong.Parse(parts[1]);
+    freshRanges.Add((low, high));
+};
+
+foreach(string line in File.ReadAllLines("input.txt"))
+{
+    processLine(line);
+}
+
+Console.WriteLine(ids.Count(IsFresh));
+
+bool IsFresh(ulong id) => freshRanges.Any(range => id >= range.low && id <= range.high);
---
+++ 2025/Day 05/2025-05-02/Program.cs
@@ -0,0 +1,40 @@
+﻿List<(ulong low, ulong high)> freshRanges = new List<(ulong low, ulong high)>();
+
+foreach(string line in File.ReadAllLines("input.txt"))
+{
+    if (string.IsNullOrWhiteSpace(line))
+    {
+        break;
+    }
+    string[] parts = line.Split('-');
+    ulong low = ulong.Parse(parts[0]);
+    ulong high = ulong.Parse(parts[1]);
+    freshRanges.Add((low, high));
+}
+
+List<(ulong low, ulong high)> mergedRanges = new List<(ulong low, ulong high)>();
+
+foreach (var range in freshRanges.OrderBy(r => r.low))
+{
+    if (mergedRanges.Count == 0)
+    {
+        mergedRanges.Add(range);
+    }
+    else
+    {
+        var last = mergedRanges.Last();
+        if (range.low <= last.high + 1)
+        {
+            mergedRanges[mergedRanges.Count - 1] = (last.low, Math.Max(last.high, range.high));
+        }
+        else
+        {
+            mergedRanges.Add(range);
+        }
+    }
+}
+
+Console.WriteLine(mergedRanges.Sum(RangeSize));
+
+long RangeSize((ulong low, ulong high) input) => (long)(input.high - input.low + 1);
+
