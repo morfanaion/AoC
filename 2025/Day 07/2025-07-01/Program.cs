92cb9a650a1e6d16042a23280003103d08edf7de
morfanaion <noordzij.am@gmail.com>
2025-12-07 08:43:28 +0100

    Day 7 2025

---
+++ 2025/Day 07/2025-07-01/Program.cs
@@ -0,0 +1,41 @@
+﻿string[] input = File.ReadAllLines("input.txt");
+List<int> beamIds = new List<int>() { input[0].IndexOf('S') };
+List<List<int>> splitterIds = new List<List<int>>();
+foreach(string line in input.Skip(0))
+{
+    if(!line.Any(c => c == '^'))
+    {
+        continue;
+    }
+    List<int> myIds = new List<int>();
+    string processingLine = line;
+    int startingIdx = 0;
+    while(processingLine.Any(c => c == '^'))
+    {
+        int newIdx = processingLine.IndexOf('^');
+        myIds.Add(newIdx +  startingIdx);
+        startingIdx+= newIdx + 1;
+        processingLine = processingLine.Substring(newIdx +1);
+    }
+    splitterIds.Add(myIds);
+}
+int numSplits = 0;
+foreach(List<int> splitterIdsForLevel in splitterIds)
+{
+    List<int> newBeamIds = new List<int>();
+    foreach(int beamId in beamIds)
+    {
+        if (splitterIdsForLevel.Any(i => i == beamId))
+        {
+            newBeamIds.Add(beamId - 1);
+            newBeamIds.Add(beamId + 1);
+            numSplits++;
+        }
+        else
+        {
+            newBeamIds.Add(beamId);
+        }
+    }
+    beamIds = newBeamIds.Distinct().ToList();
+}
+Console.WriteLine(numSplits);
---
+++ 2025/Day 07/2025-07-02/Program.cs
@@ -0,0 +1,69 @@
+﻿string[] input = File.ReadAllLines("input.txt");
+Dictionary<int, long> beamIds = new Dictionary<int, long>
+{
+    { input[0].IndexOf('S'), 1 }
+};
+List<List<int>> splitterIds = new List<List<int>>();
+foreach (string line in input.Skip(0))
+{
+    if (!line.Any(c => c == '^'))
+    {
+        continue;
+    }
+    List<int> myIds = new List<int>();
+    string processingLine = line;
+    int startingIdx = 0;
+    while (processingLine.Any(c => c == '^'))
+    {
+        int newIdx = processingLine.IndexOf('^');
+        myIds.Add(newIdx + startingIdx);
+        startingIdx += newIdx + 1;
+        processingLine = processingLine.Substring(newIdx + 1);
+    }
+    splitterIds.Add(myIds);
+}
+int numSplits = 0;
+foreach (List<int> splitterIdsForLevel in splitterIds)
+{
+    Dictionary<int, long> newBeamIds = new Dictionary<int, long>();
+    foreach (KeyValuePair<int, long> beamId in beamIds)
+    {
+        if (splitterIdsForLevel.Any(i => i == beamId.Key))
+        {
+            if(newBeamIds.ContainsKey(beamId.Key - 1))
+            {
+                newBeamIds[beamId.Key - 1] += beamId.Value;
+            }
+            else
+            {
+                newBeamIds[beamId.Key - 1] = beamId.Value;
+            }
+            if (newBeamIds.ContainsKey(beamId.Key + 1))
+            {
+                newBeamIds[beamId.Key + 1] += beamId.Value;
+            }
+            else
+            {
+                newBeamIds[beamId.Key + 1] = beamId.Value;
+            }
+        }
+        else
+        {
+            if(newBeamIds.ContainsKey(beamId.Key))
+            {
+                newBeamIds[beamId.Key] += beamId.Value;
+            }
+            else
+            {
+                newBeamIds[beamId.Key] = beamId.Value;
+            }
+        }
+    }
+    beamIds = newBeamIds;
+}
+long total = 0;
+foreach(long count in beamIds.Values)
+{
+    total += count;
+}
+Console.WriteLine(total);
