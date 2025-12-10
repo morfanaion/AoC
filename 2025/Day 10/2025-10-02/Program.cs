3751c812238e5a4674ca632c0f28183d55341128
André Noordzij <a.noordzij@chipsoft.com>
2025-12-10 13:12:24 +0100

    Forgot to do the output for Day 10 part 2

--- 2025/Day 10/2025-10-02/Program.cs
+++ 2025/Day 10/2025-10-02/Program.cs
@@ -1,5 +1,4 @@
 ﻿using _2025_10_02;
 
 List<Machine> machines = File.ReadAllLines("input.txt").Select(Machine.FromString).ToList();
-int result = 0;
-Console.WriteLine(result);
+Console.WriteLine(machines.Sum(m => m.FindMinNumPressesToGetTargeJoltages()));
