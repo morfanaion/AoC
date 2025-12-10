8e9d9c4a2761f14c2f515b5cecc9c3b292ab9884
André Noordzij <a.noordzij@chipsoft.com>
2025-12-10 10:26:20 +0100

    Day 10 part 1

---
+++ 2025/Day 10/2025-10-01/Machine.cs
@@ -0,0 +1,125 @@
+﻿
+
+namespace _2025_10_01
+{
+    internal class Machine
+    {
+        public static Machine FromString(string input)
+        {
+            Machine machine = new Machine();
+            while(input.Length > 0)
+            {
+                switch (input[0])
+                {
+                    case '[':
+                        (machine.IndicatorLightTarget, input) = ProcessIndicatorLights(input);
+                        break;
+                    case '(':
+                        (ushort button, input) = ProcessButton(input);
+                        machine.Buttons.Add(button);
+                        break;
+                    case '{':
+                        input = input.Substring(input.IndexOf('}') + 1);
+                        break;
+                    default:
+                        input = input.Substring(1);
+                        break;
+                }
+                 
+            }
+            return machine;
+        }
+
+        private static (ushort button, string input) ProcessButton(string input)
+        {
+            int indexOfClosingParantheses = input.IndexOf(')');
+            ushort button = 0;
+            foreach(ushort indicatorIdx in input.Substring(1, indexOfClosingParantheses - 1).Split(',').Select(ushort.Parse))
+            {
+                button |= (ushort)(1 << indicatorIdx);
+            }
+            
+            return (button, input.Substring(indexOfClosingParantheses + 1));
+        }
+
+        private static (ushort indicatorLightTarget, string input) ProcessIndicatorLights(string input)
+        {
+            int indexOfClosingBracket = input.IndexOf(']');
+            ushort indicatorTarget = 0;
+            for (int i = 0; i < indexOfClosingBracket - 1; i++)
+            {
+                if (input[i + 1] == '#')
+                {
+                    indicatorTarget |= (ushort)(1 << i);
+                }
+            }
+            return (indicatorTarget, input.Substring(indexOfClosingBracket + 1));
+        }
+
+        public ushort IndicatorLightTarget { get; set; } = 0;
+        
+        public List<ushort> Buttons { get; } = new List<ushort>();
+
+        public int FindMinNumPressesToGetTargetIndicators()
+        {
+            PriorityQueue<ushort, int> priorityQueue = new PriorityQueue<ushort, int>();
+            foreach(ushort button in Buttons)
+            {
+                priorityQueue.Enqueue(button, 1);
+            }
+            while(priorityQueue.TryDequeue(out ushort currentIndicator, out int currentPresses))
+            {
+                if(currentIndicator == IndicatorLightTarget)
+                {
+                    return currentPresses;
+                }
+                foreach(ushort button in Buttons)
+                {
+                    priorityQueue.Enqueue((ushort)(currentIndicator ^ button), currentPresses + 1);
+                }
+            }
+            return 0;
+        }
+
+        public override string ToString()
+        {
+            string result = "[";
+
+            ushort indicatorLightTarget = IndicatorLightTarget;
+            while(indicatorLightTarget != 0)
+            {
+                if((indicatorLightTarget & 1) == 1)
+                {
+                    result += '#';
+                }
+                else
+                {
+                    result += '.';
+                }
+                indicatorLightTarget >>= 1;
+            }
+            result += "] ";
+
+            foreach (ushort button in Buttons)
+            {
+                result += "(";
+                bool first = true; ;
+                for (int i = 0; i < 8; i++)
+                {
+                    if (((button >> i) & 1) == 1)
+                    {
+                        if (!first)
+                        {
+                            result += ',';
+                        }
+                        result += i.ToString();
+                        first = false;
+                    }
+                }
+                result += ") ";
+            }
+
+            return result;
+        }
+    }
+}
---
+++ 2025/Day 10/2025-10-01/Program.cs
@@ -0,0 +1,7 @@
+﻿using _2025_10_01;
+
+List<Machine> machines = File.ReadAllLines("input.txt").Select(Machine.FromString).ToList();
+
+File.WriteAllLines("output.txt", machines.Select(m => m.ToString()));
+
+Console.WriteLine(machines.Sum(m => m.FindMinNumPressesToGetTargetIndicators()));
